9adcaeebc65d66ade5dfe8969c37850b407ef93a
Lucas Picinini <137008535+lucaspicinini@users.noreply.github.com>
2024-02-03 12:09:21 -0300

    fix bug on switch qr and algo

--- app/index.js
+++ app/index.js
@@ -38,11 +38,11 @@ function changeBackground() {
 
 // configurações de codificação ---------->
 // alterna entre os modos QRCode e Decodificação
-function alternaQRCodeDecodificacao() {
-    if (globals.containerAlgoritmos.classList.contains("d-none") && !globals.containerQRCode.classList.contains("d-none")) {
+function alternaQRCodeDecodificacao(boolean) {
+    if (boolean == false) {
         globals.containerAlgoritmos.classList.remove("d-none")
         globals.containerQRCode.classList.add("d-none")
-    } else if (globals.containerQRCode.classList.contains("d-none") && !globals.containerCards.classList.contains("d-none")) {
+    } else if (boolean == true){
         globals.containerQRCode.classList.remove("d-none")
         globals.containerAlgoritmos.classList.add("d-none")
     }
@@ -53,7 +53,7 @@ const algoritmoEmUso = definirAlgoritmoEmUso(globals.algoritmoAlurithm)
 
 // seleciona o Alurithm, algoritmo default do challenge
 globals.btnAlurithm.addEventListener("click", () => {
-    alternaQRCodeDecodificacao()
+    alternaQRCodeDecodificacao(false)
     definirAlgoritmoEmUso(globals.algoritmoAlurithm)
     globals.algoBannerAlurithm.classList.remove("d-none")
     globals.algoBannerCustom.classList.add("d-none")
@@ -70,7 +70,7 @@ globals.btnPersonalizado.addEventListener("click", () => {
         if(validacao() == false){
             setTimeout(() => globals.btnDefinirAlgo.classList.remove("disabled"), 1000)
         } else if(validacao() == true) {
-            alternaQRCodeDecodificacao()
+            alternaQRCodeDecodificacao(false)
             const algoritmoPersonalizado = formarAlgoritmoPersonalizado()
             definirAlgoritmoEmUso(algoritmoPersonalizado)
             globals.algoBannerCustom.classList.remove("d-none")
@@ -83,7 +83,7 @@ globals.btnPersonalizado.addEventListener("click", () => {
 
 // alterna para o modo de codificação em QRCode
 globals.btnQRCode.addEventListener("click", () => {
-    alternaQRCodeDecodificacao()
+    alternaQRCodeDecodificacao(true)
     globals.algoBannerQRCode.classList.remove("d-none")
     globals.algoBannerCustom.classList.add("d-none")
     globals.algoBannerAlurithm.classList.add("d-none")
